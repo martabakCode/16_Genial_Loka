033ef86e52fd4e99f8cfa776e6a5d0dc53519867
samuelandrey <samuelandrey2021@gmail.com>
2024-04-27 11:07:18 +0700

    add:view index location

--- config/adminlte.php
+++ config/adminlte.php
@@ -359,7 +359,7 @@ return [
 
     'plugins' => [
         'Datatables' => [
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',
